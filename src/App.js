3def57f985cdb552b9279379d464697711fb8cdb
Camila Rivero <camila.rivero@swissmedical.com.ar>
2023-10-04 11:59:55 -0300

    pantalla inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import ruleta from './imgs/ruleta.png'
+import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>
+        La ruleta rusa del Home Office
+      </h1>
+      <img src={ruleta} className="ruleta" alt="ruleta" />
     </div>
   );
 }
